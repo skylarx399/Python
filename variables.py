738657289768e712c863705a53d2a1f97b054d4b
skylarx399 <111073550+skylarx399@users.noreply.github.com>
2022-10-29 09:17:58 +0530

    Create variables.py

---
+++ variables.py
@@ -0,0 +1,26 @@
+#comments
+
+#varirables
+  print('skylarx')
+  
+  name = 'skylarx'
+  age = 20
+  
+  print(name)
+  print(age)
+  
+  full_name = 'skylarx me'
+  print(full_name)
+  
+  width , height = 400,500
+  print(width)
+  print(height)
+  
+  your_name = input('please enter your name: ')
+  print('hi my name is ' + your_name)
+  your_age = input('please enter your age: ')
+  print('And my age is ' + your_age)
+  
+  num1 = int(input('enter your number: '))
+  num2 = int(input('enter your number: '))
+  print(int(num1) +int(num2))
