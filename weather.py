2ca1daee912962d19ec28fabca06d60ce07ff8dc
skylarx399 <111073550+skylarx399@users.noreply.github.com>
2022-10-29 09:19:23 +0530

    Create weather.py

---
+++ weather.py
@@ -0,0 +1,20 @@
+# baby weather app
+
+weather = input('How is the weather? ')
+#weather = 'sunny'
+#weather = 'rainy'
+
+if weather == 'rainy':
+  print('☔')
+
+elif weather == 'cloudy':
+  print('☁')
+
+elif weather == 'thunderstorm':
+  print('🌩')
+
+elif weather == 'snowy':
+  print('☃️')
+ 
+else:
+  print('😎')
